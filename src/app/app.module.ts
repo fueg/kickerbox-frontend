4a92b7eeaa4ad1ca9e7500190217ce73e35a940d
fueg <Sschrei5>
2018-06-26 11:33:04 +0200

    add components for navigation and reservations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {ReservationsComponent} from './reservations/reservations.component';
+import {ResultsComponent} from './results/results.component';
+
+const appRoutes: Routes = [
+  {path: 'reservations', component: ReservationsComponent},
+  {path: 'results', component: ResultsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ReservationsComponent,
+    ResultsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // debugging
+    ),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
