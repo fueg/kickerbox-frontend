060725e738e34fa7a24c4aaddc159f44f36b4a65
jofr <jonas.frehner@zuehlke.com>
2018-06-29 13:53:15 +0200

    Add basic shortcut-functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private router: Router, private location: Location) {
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyPress(event: KeyboardEvent) {
+    const currentRoute = this.router.url;
+    if (currentRoute.substr(1, currentRoute.length).includes('/')) {
+      if (event.key === 'c') {
+        this.location.back();
+      }
+    }
+    if (event.key === 'n') {
+      this.router.navigate(['reservations/new']);
+    }
+    if (event.key === 'r') {
+      this.router.navigate(['results/new']);
+    }
+    if (event.key === 't') {
+      this.router.navigate(['teams/new']);
+    }
+  }
 }
